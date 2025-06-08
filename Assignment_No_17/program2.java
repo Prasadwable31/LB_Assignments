c374cd783c3e812e0929dd97d0200cba1d9292dd
Prasad Wable <prasadwable31@gmail.com>
2025-06-08 16:59:29 +0530

    Add files via upload

---
+++ Assignment_No_17/program2.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow, int iCol)
+    {
+        int i = 0, j = 0;
+
+        if(iRow != iCol)
+        {
+            return;
+        }
+        
+        for(i = 1; i <= iRow; i++)
+        {
+            for(j = iCol; j >= 1; j--)
+            {
+                if(i == j)
+                {
+                    System.out.print("# ");
+                }
+                else if(i < j)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("@ ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class program2
+{
+    public static void main(String arr[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of rows : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+    }
+}
